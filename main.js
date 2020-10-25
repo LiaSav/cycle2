2116376b6cb3bb6161a34b47232cbaa292faadd6
LiaSav <71651252+LiaSav@users.noreply.github.com>
2020-10-25 21:56:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // The function returns a string that consists of N numbers repeated K times, separated by a space.
-function testCycle(k, n) {
+function cycle(k, n) {
   let x = "";
   for (let i = 1; i <= k; i++) {
     x += n + " ";
